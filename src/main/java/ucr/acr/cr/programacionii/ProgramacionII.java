8a2b211417787a12ddd2905b04eef4ee568511bd
Ariel <Ariel@DESKTOP-P2BKHFJ>
2024-03-15 08:36:11 -0600

    Primera carga

---
+++ src/main/java/ucr/acr/cr/programacionii/ProgramacionII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.acr.cr.programacionii;
+
+/**
+ *
+ * @author Ariel
+ */
+public class ProgramacionII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
